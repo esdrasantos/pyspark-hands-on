595e3ca1fb3abefb17e2d73ba0a1be9ecf0678ec
Esdra <69021404+esdrasantos@users.noreply.github.com>
2025-12-14 13:35:54 +0000

    MOVED: test_mysql_spark.py

---
+++ test_mysql_spark.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder
+    .appName("Spark-MySQL")
+    .config(
+        "spark.jars",
+        "libs/mysql-connector-j-9.5.0/mysql-connector-j-9.5.0.jar"
+    )
+    .getOrCreate()
+)
+
+spark.sparkContext.setLogLevel("ERROR")
+
+df_mysql = (
+    spark.read
+    .format("jdbc")
+    .option(
+        "url",
+        "jdbc:mysql://mysql:3306/demol"
+        "?useSSL=false&allowPublicKeyRetrieval=true"
+    )
+    .option("dbtable", "demotable")
+    .option("user", "root")
+    .option("password", "root")
+    .option("driver", "com.mysql.cj.jdbc.Driver")
+    .load()
+)
+
+df_mysql.show()
