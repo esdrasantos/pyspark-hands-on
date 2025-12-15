f7d7dbb86c88bffd674170dabfe926de12c28647
Esdra <69021404+esdrasantos@users.noreply.github.com>
2025-12-15 12:35:56 +0000

    CHANGED: Reading CSV file and creating pyspark dataframe

--- test_mysql_spark.py
+++ test_mysql_spark.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import Row
+import pandas as pd 
 
 spark = (
     SparkSession.builder
@@ -11,6 +13,10 @@ spark = (
 )
 
 spark.sparkContext.setLogLevel("ERROR")
+
+print('Spark Version:', spark.sparkContext.version)
+
+# Reading dataframe from demol:demotable
 
 df_mysql = (
     spark.read
@@ -32,3 +38,36 @@ df_mysql.show()
 
 print('Schema:')
 df_mysql.printSchema()
+
+print('Creating new schema for People')
+sc = spark.sparkContext
+people_list = [('Karol',27), ('Maria', 8)]
+people = [Row(name=name, age=int(age)) for name, age in people_list]
+schema_people = spark.createDataFrame(people)
+
+print(type(schema_people))
+
+'''
+my_list = [['a',1,2], ['b',2,3], ['c',3,4]]
+col_names = ['A', 'B', 'C']
+df2 = pd.DataFrame(my_list, columns=col_names)
+
+'''
+
+# Ways to read csv in Spark 2+ , without com.databricks:spark-csv
+
+dummy = spark.read.csv(
+    "dummy1.csv",
+    header=True,
+    inferSchema=True
+)
+
+print('Dummy (CSV) Schema:')
+dummy.printSchema()
+
+print('Dummy Dataframe:')
+dummy.show()
+
+print('Display o Dummy head row (1st one)')
+print(dummy.head())
+
